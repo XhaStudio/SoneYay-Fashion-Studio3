8eee0f4564b71b9993d0814746eead4ac8588e22
Xhazlan <hmystudio7@gmail.com>
2026-09-13 19:57:26 +0630

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O cloudflared
-chmod +x cloudflared
-
-# Tunnel ကို background မှာ run ပြီး URL ကို log file ထဲ ရေးမယ်
-./cloudflared tunnel --url http://localhost:3095 --logfile tunnel.log &
-
-python app.py
